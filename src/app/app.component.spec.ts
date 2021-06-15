f47fc8b4828baa14f5a2891522b0d934ad0511f8
Austin <austin@auzt.in>
2021-06-15 15:53:02 -0700

    Add dev test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'penny-fe-dev-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('penny-fe-dev-test');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('penny-fe-dev-test app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'penny-fe-dev-test';
+export class AppComponent implements OnInit{
+  title: string = 'PENNY FE DEV TEST';
+  loading: boolean = false;
+
+  ngOnInit() {
+    this.loading = true;
+    setTimeout(() => this.loading = false, 5000)
+  }
 }
